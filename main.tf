b7214357e9b83d465fa09a314869408c52904a4b
Dave Franco <davefranco1987@gmail.com>
2020-01-29 22:05:10 -0300

    tf files moved

--- main.tf
+++
@@ -1,9 +0,0 @@
-provider "oci" {
-    version = "3.27"
-    tenancy_ocid = var.tenancy_ocid
-    user_ocid = var.user_ocid
-    fingerprint = var.fingerprint
-    private_key_path = var.private_key_path
-    region = var.region
-}
-
