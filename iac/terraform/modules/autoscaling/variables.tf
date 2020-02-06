77a4c735aa883396c997bbe2002eec0933eaf640
djfranco <djfranco@DJFRANCO-CL.cl.oracle.com>
2020-02-06 19:38:29 -0300

    move modules to simple tf files

--- iac/terraform/modules/autoscaling/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "compartment_id" {}
-variable "net_ids" { type = list(string) }
-variable "ssh_public_key" {}
-variable "vmImgID" {}
-variable "availability_domain" {}
-
--- iac/terraform/modules/oke/output.tf
+++
@@ -1,3 +0,0 @@
-output "k8s_ocid" {
-  value = oci_containerengine_cluster.k8s_cluster.id
-}
