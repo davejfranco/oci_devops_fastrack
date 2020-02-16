e0c2c5b35188ac991a8e182a4b25c5a45638d247
djfranco <djfranco@DJFRANCO-CL.cl.oracle.com>
2020-02-16 10:35:07 -0300

    some variables not required on resource manager

--- iac/terraform/variables.tf
+++ iac/terraform/variables.tf
@@ -1,8 +1,7 @@
-
-variable "tenancy_ocid" {}
+/* variable "tenancy_ocid" {}
 variable "user_ocid" {}
 variable "fingerprint" {}
-variable "private_key_path" {}
+variable "private_key_path" {} */
 
 #Change me according to your tenant
 variable "region" { default = "" }
